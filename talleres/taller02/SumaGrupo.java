3332cdc5218e5588738dcda6b6920af6785bf31a
jacarvajao <39866354+jacarvajao@users.noreply.github.com>
2019-01-29 14:01:24 -0500

    Ejercicio 2

---
+++ talleres/taller02/SumaGrupo.java
@@ -0,0 +1,8 @@
+public class SumaGrupo{
+    public static boolean SumaGrupo(int start, int[], int target ){
+        if(starte==nums.lenght){
+            return target == 0;
+            return groupSum(start+1, nums, target) || groupSum(start+1, nums, target-nums[start]);
+        }
+    }
+}
