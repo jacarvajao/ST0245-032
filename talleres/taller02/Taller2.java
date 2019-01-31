9a6bc11dcf27d58251da260ea98556bb0454a406
atorog <46933140+atorog@users.noreply.github.com>
2019-01-30 20:19:53 -0500

    Add files via upload

---
+++ talleres/taller02/Taller2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller2;
+
+/**
+ *
+ * @author casa
+ */
+public class Taller2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(GCD.gcd(150, 200));
+        SumaGrupo suma=new SumaGrupo();
+         int nums = new int[10];
+        suma.groupSum(0, nums, 10);
+        combinaciones.combinations("abc");
+    }
+    
+    
+}
