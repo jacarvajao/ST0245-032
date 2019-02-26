322878e954c77ac9d2d6eb8ca7c5ede6f3b9e2d8
jacarvajao <jacarvajao>
2019-02-26 18:19:36 -0500

    Clase 2

---
+++ talleres/taller06/MiArrayList.java
@@ -0,0 +1,30 @@
+
+public class MiArrayList {
+	private int size;
+	private static final int DEFAULT_CAPACITY = 10;
+	private int elements[];
+	
+	public MiArrayList(){
+		size = 0;
+		elements = new int[DEFAULT_CAPACITY];
+		
+	}
+	public int size(){
+		return size;
+	}
+	public void add(int e) {
+		elements[size]=e;
+		size = size+1;
+	}
+	public void add(int index, int e) throws ArrayIndexOutOfBoundsException{
+		if(index<=size) {
+			for(int i = size+1;i>index;i--) {
+				elements[i]=elements[i-1];
+			}
+			elements[index]=e;
+		}else {
+			throw new ArrayIndexOutOfBoundsException("Usuario el index sobrepasa el tamaño");
+		}
+	}
+	//
+}
